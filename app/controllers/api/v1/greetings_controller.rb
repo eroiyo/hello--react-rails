f6de9d2431f0fc20e44491c82cb64bc72c3522f5
eroiyo <eroiyo@outlook.es>
2021-12-08 17:40:07 -0400

    fix rubocop linters

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,8 +1,14 @@
-class Api::V1::GreetingsController < ApplicationController
-  def random
-    count = Greeting.count 
-    random_offset = rand(count)
-    random_salute = Greeting.offset(random_offset).first
-    render json: random_salute.salute
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class GreetingsController < ApplicationController
+      def random
+        count = Greeting.count
+        random_offset = rand(count)
+        random_salute = Greeting.offset(random_offset).first
+        render json: random_salute.salute
+      end
+    end
   end
 end
